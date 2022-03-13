ef7e6d70ca3c7656965c80baf0630b8504df9d3d
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-03-14 00:41:35 +0530

    Refactor all uc123 to in single Generic method

--- src/com/bridgelabz/generic/MaxTest.java
+++ src/com/bridgelabz/generic/MaxTest.java
@@ -6,29 +6,34 @@ public class MaxTest {
 
 	@Test
 	public void findIntMax() {
+		
+		GenericCal gc = new GenericCal();
+		
 		int a = (int) (Math.random() * 1000);
 		int b = (int) (Math.random() * 1000);
 		int c = (int) (Math.random() * 1000);
 		
-		System.out.println(GenericCal.findMax(a, b, c));
+		System.out.println(gc.findMax(a, b, c));
 	}
 	
 	@Test
 	public void findFloatMax() {
+		GenericCal gc = new GenericCal();
 		float a = (float) (Math.random() * 1000);
 		float b = (float) (Math.random() * 1000);
 		float c = (float) (Math.random() * 1000);
 		
-		System.out.println(GenericCal.findMax(a, b, c));
+		System.out.println(gc.findMax(a, b, c));
 	}
 	
 	@Test
 	public void findStringMax() {
+		GenericCal gc = new GenericCal();
 		String a = "Apple";
 		String b = "Peach";
 		String c = "Banana";
 		
-		System.out.println(GenericCal.findMax(a, b, c));
+		System.out.println(gc.findMax(a, b, c));
 	}
 
 }
